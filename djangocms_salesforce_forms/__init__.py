5d1aee66e02f6ef3626fddada8fef9695b5e7017
Filipe Waitman <filwaitman@gmail.com>
2018-01-10 15:23:00 -0200

    Bumped version to 1.0.1

--- djangocms_salesforce_forms/__init__.py
+++ djangocms_salesforce_forms/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, print_function, division
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __doc__ = (
     'Django CMS plugin based forms for submission to '
     'SalesForce Marketing Cloud (SFMC)'
