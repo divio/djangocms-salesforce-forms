cdf8ae7a0136c2d754d49d8690a0163eff355107
Filipe Waitman <filwaitman@gmail.com>
2018-01-12 13:56:55 -0200

    Only add defaults on new instances

--- djangocms_salesforce_forms/forms.py
+++ djangocms_salesforce_forms/forms.py
@@ -2,10 +2,13 @@ from aldryn_forms.forms import FormPluginForm
 
 
 class SalesforcePluginForm(FormPluginForm):
+
     defaults = {
+        'action_backend': 'none',
         'form_template': 'aldryn_forms/salesforceform/form.html',
     }
 
     def __init__(self, *args, **kwargs):
-        kwargs.setdefault('initial', {}).update(self.defaults)
+        if not kwargs.get('instance'):
+            kwargs.setdefault('initial', {}).update(self.defaults)
         super(SalesforcePluginForm, self).__init__(*args, **kwargs)
